8742ddf1af36ac7ad08456fbd4ea6201b4b401c4
msl8r <matt.slater@hmcts.net>
2022-05-20 10:09:59 +0100

    DTSPO-8049 - add initial config for sds keda mi

---
+++ state.tf
@@ -0,0 +1,17 @@
+terraform {
+
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.7.0"
+    }
+  }
+
+  required_version = "~> 1.2.0"
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "product" {
+  description = "The name of your application"
+  default     = "keda"
+}
+
+variable "env" {
+  description = "The deployment environment (sandbox, aat, prod etc..)"
+}
+
+variable "location" {
+  description = "The location where you would like to deploy your infrastructure"
+  default     = "UK South"
+}
+
+variable "common_tags" {
+  type = map(string)
+}
